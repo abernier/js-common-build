6cf676cf32ed73fd4209bf1b11096313e0b9a187
Antoine BERNIER <antoine.bernier@cafe.com>
2011-06-18 19:17:51 +0200

    Removing IE6 odd capabilities.

--- css.mk
+++ css.mk
@@ -3,19 +3,12 @@ CSS_DIR ?= theme
 CSS_SRC ?= ${CSS_DIR}
 CSS_DIST ?= ${DIST_DIR}/${CSS_DIR}
 CSS_CONCAT = ${CSS_DIST}/${PACKAGE}.css
-CSS_IE6 = ${CSS_DIST}/${PACKAGE}.ie6.css
 
 CSS_CAT = $(addprefix ${CSS_SRC}/,${CSS})
 CSS_OPT = $(addprefix ${CSS_SRC}/,${OPTIONAL_CSS})
 CSS_ALL = ${CSS_CAT} ${CSS_OPT}
 
-# s/\:(hover|focus)/.\1/g;
-IE6CSS = sed -r "s/(@import.+)\.css/\1.ie6.css/g; s/\[([a-z\-]+)\]/.\1/g; s/\[([a-z\-]+)~?=([a-z]+)\]/.\1-\2/g"
-
-%.ie6.css: %.css
-	@@$(IE6CSS) $< | grep -Fv "*IE6*" > $@
-
-css: ${CSS_CONCAT} ${CSS_IE6}
+css: ${CSS_CONCAT}
 
 ifdef OPTIONAL_CSS
 optcss: ${CSS_DIST} ${MODE_FILE} ${CSS_OPT}
